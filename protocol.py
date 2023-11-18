263820612789fdc23195d2889bd6034d2781b073
dilyarakhannanova <diliara700@mail.ru>
2023-11-18 10:51:24 +0300

    add sent_window add resend last ack

--- protocol.py
+++ protocol.py
@@ -33,31 +33,39 @@ class Segment:
 
 
 HEADER_SIZE = 8
-MAX_DATA_SIZE = 2**15 - 8
+MAX_DATA_SIZE = 2**15 - HEADER_SIZE
 
 class MyTCPProtocol(UDPBasedProtocol):
     def __init__(self, *args, **kwargs):
         self.seq_num = 0
         self.ack_num = 0
         self._received = []
+        self._wait_acks = []
         super().__init__(*args, **kwargs)
-        self.udp_socket.settimeout(0.01)
+        self.udp_socket.settimeout(0.001)
 
     def send(self, data: bytes):
         bytes_sent = 0
         while bytes_sent < len(data):
-            max_size = min(len(data), MAX_DATA_SIZE - 8 + bytes_sent)
+            max_size = min(len(data), MAX_DATA_SIZE + bytes_sent)
             segment = Segment(self.seq_num, self.ack_num, data[bytes_sent: max_size])
             self.seq_num += len(segment.data)
             self.sendto(segment.dumps())
+            self._wait_acks.append(segment)
             bytes_sent = max_size
-            while True:
-                try:
-                    answer = Segment.loads(self.recvfrom(8))
-                    if answer.ack_num >= self.seq_num:
-                        break
-                except socket.error:
+        while len(self._wait_acks):
+            try:
+                answer = Segment.loads(self.recvfrom(HEADER_SIZE))
+                while len(self._wait_acks) and answer.ack_num >= (self._wait_acks[0].seq_num + len(self._wait_acks[0].data)):
+                    self._wait_acks.pop(0)
+            except socket.error:
+                for segment in self._wait_acks:
                     self.sendto(segment.dumps())
+                    if len(self._wait_acks) == 1:
+                        for _ in range(10):
+                            self.sendto(segment.dumps())
+                        self._wait_acks.pop(0)
+
 
         return len(data)
 
@@ -65,7 +73,7 @@ class MyTCPProtocol(UDPBasedProtocol):
         data = b""
         while len(data) < n:
             try:
-                segment = Segment.loads(self.recvfrom(n + 8))
+                segment = Segment.loads(self.recvfrom(MAX_DATA_SIZE + HEADER_SIZE))
                 self._received.append(segment)
             except socket.error:
                 self._ack()
@@ -76,11 +84,13 @@ class MyTCPProtocol(UDPBasedProtocol):
                     if self.ack_num == self._received[0].seq_num:
                         self.ack_num = self._received[0].seq_num + len(self._received[0].data)
                         data += self._received[0].data
-                        self._ack()
                     self._received.pop(0)
                 else:
                     break
 
+            self._ack()
+        for _ in range(10):
+            self._ack()
         return data
 
     def _ack(self):
